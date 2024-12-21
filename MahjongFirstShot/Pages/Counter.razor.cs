e96780da5c085cd7cd35999c7f70c1d5f6f87ee0
green <green.grosjean@gmail.com>
2024-12-21 12:18:57 +0900

    Correct sorting to correct order

--- MahjongFirstShot/Pages/Counter.razor.cs
+++ MahjongFirstShot/Pages/Counter.razor.cs
@@ -2,57 +2,20 @@
 
 partial class Counter
 {
-    private readonly List<string> BaseItems = [];
-
-    private readonly List<string> Items = [];
+    private readonly List<int> Items = [];
 
     public Counter()
     {
-        BaseItems.Add("一");
-        BaseItems.Add("二");
-        BaseItems.Add("三");
-        BaseItems.Add("四");
-        BaseItems.Add("五");
-        BaseItems.Add("六");
-        BaseItems.Add("七");
-        BaseItems.Add("八");
-        BaseItems.Add("九");
-
-        BaseItems.Add("①");
-        BaseItems.Add("②");
-        BaseItems.Add("③");
-        BaseItems.Add("④");
-        BaseItems.Add("⑤");
-        BaseItems.Add("⑥");
-        BaseItems.Add("⑦");
-        BaseItems.Add("⑧");
-        BaseItems.Add("⑨");
-
-        BaseItems.Add("１");
-        BaseItems.Add("２");
-        BaseItems.Add("３");
-        BaseItems.Add("４");
-        BaseItems.Add("５");
-        BaseItems.Add("６");
-        BaseItems.Add("７");
-        BaseItems.Add("８");
-        BaseItems.Add("９");
-
         for (int i = 1; i <= 4; i++)
         {
-            foreach (var item in BaseItems)
-            {
-                Items.Add(item);
-            }
+            Items.AddRange(Enumerable.Range(1, 27));
         }
     }
 
-    private List<string> ShuffleItems()
+    private IEnumerable<int> ShuffleItems()
     {
         var random = new Random();
 
-        var shuffledItems = Items.OrderBy(x => random.Next()).ToList();
-
-        return shuffledItems;
+        return Items.OrderBy(x => random.Next());
     }
 }
