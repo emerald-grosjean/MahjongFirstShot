0c16dc5f91e4ac3676f9762ad8dd46d284ae77e0
green <green.grosjean@gmail.com>
2024-12-24 15:42:06 +0900

    Add Dora

---
+++ MahjongFirstShot/Models/Wall.cs
@@ -0,0 +1,18 @@
+﻿using MahjongFirstShot.Utils;
+
+namespace MahjongFirstShot.Models;
+
+public class Wall
+{
+    public readonly IReadOnlyList<int> TileIds = DealerUtils.ShuffleTileIds().ToList().AsReadOnly();
+
+    public Wall()
+    {
+        WallTileIds.AddRange(TileIds.Take(122));
+        DeadWallTileIds.AddRange(TileIds.TakeLast(14));
+    }
+
+    public List<int> WallTileIds { get; set; } = [];
+
+    public List<int> DeadWallTileIds { get; set; } = [];
+}
---
+++ MahjongFirstShot/Utils/DealerUtils.cs
@@ -0,0 +1,13 @@
+﻿namespace MahjongFirstShot.Utils;
+
+public static class DealerUtils
+{
+    private static readonly IReadOnlyList<int> TileIds = Enumerable.Range(1, 34 * 4).ToList().AsReadOnly();
+
+    public static IEnumerable<int> ShuffleTileIds()
+    {
+        var random = new Random();
+
+        return TileIds.OrderBy(x => random.Next());
+    }
+}
